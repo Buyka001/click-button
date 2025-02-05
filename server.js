801979f3217a7e5cdf75e011a8bfbe260e76df55
Buyka001 <buyandavaa@icloud.com>
2025-02-06 00:45:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(express.json());
 
 // Route to check if the server is running
