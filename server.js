d0254392b6b2399a9059d4c5976bbba181f85851
Buyka001 <buyandavaa@icloud.com>
2025-02-05 23:48:41 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/click", (req, res) => {
+    console.log("Button clicked!");
+    res.send("Button click received!");
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
